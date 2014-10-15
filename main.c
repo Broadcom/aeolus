de0f965aa24f2cf16cf03dfd4b85091647c655a7
Kevin Cernekee <cernekee@gmail.com>
2014-10-14 22:43:40 -0700

    main: Pass the builtin device tree blob to Linux
    The argument passing convention mimics ARM, as many of the supported
    product lines have both MIPS and ARM based SoCs (mostly reusing the same
    peripherals).
    
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
-void main(void (*kernel_entry)(void),
+typedef void (*kernel_entry_t)(unsigned long zero,
+			       unsigned long machine_type,
+			       unsigned long dtb_pa);
+
+#define KSEG0			0x80000000
+
+static inline unsigned long virt_to_phys(void *ptr)
+{
+	return (unsigned long)ptr - KSEG0;
+}
+
+void main(kernel_entry_t kernel_entry,
 	  char *cmdline,
 	  void *dtb_start,
 	  int dtb_len)
 {
-	kernel_entry();
+	kernel_entry(0, 0xffffffff, virt_to_phys(dtb_start));
 }
